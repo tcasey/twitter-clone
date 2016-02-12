a4df6761bb010d1dccc6e4ea0af58a942229e218
tcasey <trevort.casey@gmail.com>
2016-02-12 11:04:57 -0700

    Bug fix

--- js/style.js
+++ js/style.js
@@ -1,40 +1,36 @@
-$(document).ready(function() {
-
-  var maxCharacters = 140;
-  var characters = $('#textArea').val().length;
-/* ---------Double Height of box & show tweet controls----- */
+$(document).ready(function(){
+//Open and close the box.
+//***Need to return box to original height after tweet.***
   $('.tweet-compose').on('click', function() {
-    $(this).height("80px");
-    $('#tweet-controls').css("display", "block");
-  });
-  /*----------Return Box to original height value on some event (Added Feat.)---------*/
-  $('.tweet-compose').on('dblclick', function() {
-    $(this).height("23px");
-  })
-
-  $('#char-count').text(maxCharacters);
-/*--------changed id of textarea to make the count specific--------*/
+   $(this).height("80px");
+   $('#tweet-controls').css("display", "block");
+ });
+ $('.tweet-compose').dblclick('click', function(){
+   $(this).height("20px");
+ });
+//Twitter char-count now turns red with 20 instead of 10 remaining characters
+//The purpose of this event is to count characters and trigger the color red and grey respectively, depending on the user input.
 $('#textArea').bind('keyup keydown', function() {
-//After binding keyup and keydown, this allows the maxCharacters to determine which part of the if else if statement will work.
-var maxCharacters = 140;
-$('#char-count').text(maxCharacters);
-//  var count = $('#char-count');
- var characters = $(this).val().length;
- if (characters > maxCharacters - 20) {
-  $('#char-count').css("color", "red");
-     // $('#tweet-submit').addClass('button');
- }
- else{
-   $('#char-count').css("color", "#999");
- };
-
- if (characters > maxCharacters){
-  $('#tweet-submit').addClass('disable');
- }
- else {
-     $('#tweet-submit').removeClass('disable');
-     $('#tweet-submit').addClass('button');
+  //After binding keyup and keydown, this allows the maxCharacters to determine which part of the if else if statement will work.
+  var maxCharacters = 140;
+  $('#char-count').text(maxCharacters);
+  //  var count = $('#char-count');
+   var characters = $(this).val().length;
+   if (characters > maxCharacters - 20) {
+    $('#char-count').css("color", "red");
+       // $('#tweet-submit').addClass('button');
    }
-  $('#char-count').text(maxCharacters - characters);
+   else{
+     $('#char-count').css("color", "#999");
+   };
+
+   if (characters > maxCharacters){
+    $('#tweet-submit').addClass('disable');
+   }
+   else {
+       $('#tweet-submit').removeClass('disable');
+       $('#tweet-submit').addClass('button');
+     }
+    $('#char-count').text(maxCharacters - characters);
 });
 });
