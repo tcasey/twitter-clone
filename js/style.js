41bf943d83769a15f92884649cfd0babee5f7f34
tcasey <trevort.casey@gmail.com>
2016-02-12 14:47:37 -0700

    Reorganized quite a bit & added new features

--- js/style.js
+++ js/style.js
@@ -1,59 +1,95 @@
-$(document).ready(function(){
-//Open and close the box.
-//***Need to return box to original height after tweet.***
-  $('.tweet-compose').on('click', function() {
-   $(this).height("80px");
-   $('#tweet-controls').css("display", "block");
- });
- $('.tweet-compose').dblclick('click', function(){
-   $(this).height("20px");
- });
-//Twitter char-count now turns red with 20 instead of 10 remaining characters
-//The purpose of this event is to count characters and trigger the color red and grey respectively, depending on the user input.
-$('#textArea').bind('keyup keydown', function() {
-  //After binding keyup and keydown, this allows the maxCharacters to determine which part of the if else if statement will work.
-  var maxCharacters = 140;
-  $('#char-count').text(maxCharacters);
-  //  var count = $('#char-count');
-   var characters = $(this).val().length;
-   if (characters > maxCharacters - 20) {
-    $('#char-count').css("color", "red");
-       // $('#tweet-submit').addClass('button');
-   }
-   else{
-     $('#char-count').css("color", "#999");
-   };
+$(document).ready(function() {
+  $('#tweet-controls').hide();
+  $('.stats').hide();
+  $('.reply').hide();
+/*this grows and fades in the tweet-controls
+for when clicked*/
+  $('textarea').on('focus', function() {
+    $(this).animate({height: '5em'}, 500)
+    $(this).siblings('#tweet-controls').fadeIn();
+  })
+/*this shrinks the tweet box and hides the tweet-controls*/
 
-   if (characters > maxCharacters){
-    $('#tweet-submit').addClass('disable');
-   }
-   else {
-       $('#tweet-submit').removeClass('disable');
-       $('#tweet-submit').addClass('button');
+  $('textarea').on('blur', function() {
+    $(this).animate({height: '2.5em'}, 500)
+    $('#tweet-controls').fadeOut();
+  })
+/*creating the counter for the tweet box, and making the function*/
+$('textarea').on('keyup', function() {
+   charNum = 140 - $('textarea.tweet-compose').val().length;
+   $('#char-count').text(charNum);
+   if (charNum <= 20) {
+      $('#char-count').css({color: 'red'});
+ }
+    else if (charNum > 20) {
+      $('#char-count').css({color: '#999'});
+  }
+})
+
+$('textarea').on('keyup', function() {
+    charNum = 140 - $('textarea.tweet-compose').val().length;
+    $('#char-count').text(charNum);
+    if (charNum < 0) {
+      $('#tweet-submit').attr('disabled', 'disabled');
+    }
+     else {
+       $('#tweet-submit').removeAttr('disabled');
      }
-    $('#char-count').text(maxCharacters - characters);
-});
-//Take the content of the tweet and prepend to the top of the stream so people know what you are trying to tell them when you click tweet.
+})
+/*this prepends the new tweet into the feed
+so it can be altered and what not!*/
 
-$('#tweet-submit').on('click',function(){
-  var tweetChar = $('#textArea').val().length;
-  if(tweetChar <= 140) {
-  $('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">Trevor Casey</strong><span class="username">@tcasey</span><p class="tweet-text">'
+$('#tweet-submit').on('click', function() {
+  $('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/hat.jpg" /><strong class="fullname">Trevor Casey </strong><span class="username">@tcasey</span><p class="tweet-text">'
 
-/*-----Making the text value live----- */
    + $('textarea.tweet-compose').val() +
-
 
    '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="stats"><div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div><div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div><div class="time">'
 
-/*-----Making the time stamp live----- */
-   +  \ +
+
+   +
 
    '</div></div>');
 
-};
 
-
-});
-
-});
+  $('textarea.tweet-compose').val('');
+          /*this wilil make the tweet-actions show and hide!*/
+          $('.tweet').on('mouseenter', function() {
+            $(this).find('.tweet-actions').slideDown();
+          })
+          $('.tweet-actions').hide();
+          $('.tweet').on('mouseleave', function() {
+            $(this).find('.tweet-actions').slideUp();
+          })
+          /*show & hide retweets and timestamp*/
+          $('.stats').hide();
+          $('.tweet').on('mouseenter', function() {
+            $(this).find('.stats').slideDown();
+          })
+          $('.tweet').on('mouseleave', function() {
+            $(this).find('.stats').slideUp();
+          })
+})
+/*this crap makes the tweet-actions show and hide*/
+$('.tweet').on('mouseenter', function() {
+  $(this).find('.tweet-actions').slideDown();
+})
+$('.tweet-actions').hide();
+$('.tweet').on('mouseleave', function() {
+  $(this).find('.tweet-actions').slideUp();
+})
+/*show & hide retweets and timestamp*/
+$('.tweet').on('mouseenter', function() {
+  $(this).find('.stats').slideDown();
+})
+$('.tweet').on('mouseleave', function() {
+  $(this).find('.stats').slideUp();
+})
+/*show and hide reply box*/
+$('.tweet').on('mouseenter', function() {
+  $(this).find('.reply').slideDown();
+})
+$('.tweet').on('mouseleave', function() {
+  $(this).find('.reply').slideUp();
+})
+})
